5a68df5e76ffa1f8a17d8ca02e1da2850db56342
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-10-14 11:24:10 -0700

    v0.6.7 - changes in CHANGELOG

--- src/castle.c
+++ src/castle.c
@@ -20,10 +20,11 @@
 #include "ciphers/specjal_cbc.c"
 #include "ciphers/zywca_cbc.c"
 #include "ciphers/qapla.c"
+#include "ciphers/toQ.c"
 
 void usage() {
-    printf("DarkCastle v0.6.6.6 - by KryptoMagik\n\n");
-    printf("Algorithms:\n***********\n\ndark             256 bit\nuvajda           256 bit\nspock            256 bit\namagus           256 bit\namagus512        512 bit\namagus1024       1024 bit\nqapla            256 bit\nspecjal          256 bit\nspecjal512       512 bit\nzanderfish2-cbc  256 bit\nzanderfish2-ofb  256 bit\nzanderfish2-ctr  256 bit\nzanderfish3      256 bit\nzanderfish3-512  512 bit\nzanderfish3-1024 1024 bit\nzanderfish3-ofb  256 bit\nzywca            256 bit\n");
+    printf("DarkCastle v0.6.7 - by KryptoMagik\n\n");
+    printf("Algorithms:\n***********\n\ndark             256 bit\nuvajda           256 bit\nspock            256 bit\namagus           256 bit\namagus512        512 bit\namagus1024       1024 bit\nqapla            256 bit\ntoQ              256 bit\nspecjal          256 bit\nspecjal512       512 bit\nzanderfish2-cbc  256 bit\nzanderfish2-ofb  256 bit\nzanderfish2-ctr  256 bit\nzanderfish3      256 bit\nzanderfish3-512  512 bit\nzanderfish3-1024 1024 bit\nzanderfish3-ofb  256 bit\nzywca            256 bit\n");
     printf("Usage: castle <algorithm> <-e/-d> <input file> <output file> <password>\n\n");
 }
 
@@ -45,6 +46,7 @@ int main(int argc, char *argv[]) {
     int specjal_nonce_length = 32;
     int zywca_nonce_length = 32;
     int qapla_nonce_length = 16;
+    int toQ_nonce_length = 16;
 
     int zanderfish_key_length = 32;
     int zanderfish2_key_length = 32;
@@ -61,6 +63,7 @@ int main(int argc, char *argv[]) {
     int specjal512_key_length = 64;
     int zywca_key_length = 32;
     int qapla_key_length = 32;
+    int toQ_key_length = 32;
 
     int dark_mac_length = 32;
     int zanderfish_mac_length = 32;
@@ -72,6 +75,7 @@ int main(int argc, char *argv[]) {
     int specjal_mac_length = 32;
     int zywca_mac_length = 32;
     int qapla_mac_length = 32;
+    int toQ_mac_length = 32;
 
     int dark_bufsize = 32768;
     int uvajda_bufsize = 32768;
@@ -84,6 +88,7 @@ int main(int argc, char *argv[]) {
     int specjal_bufsize = 131072;
     int zywca_bufsize = 262144;
     int qapla_bufsize = 262144;
+    int toQ_bufsize = 262144;
     
 
     if (argc != 6) {
@@ -245,6 +250,14 @@ int main(int argc, char *argv[]) {
         }
         else if (strcmp(mode, decrypt_symbol) == 0) {
             qapla_decrypt(infile_name, outfile_name, qapla_key_length, qapla_nonce_length, qapla_mac_length, kdf_iterations, kdf_salt, password, keywrap256_ivlen, qapla_bufsize);
+        }
+    }
+    else if (strcmp(algorithm, "toQ") == 0) {
+        if (strcmp(mode, encrypt_symbol) == 0) {
+            toQ_encrypt(infile_name, outfile_name, toQ_key_length, toQ_nonce_length, toQ_mac_length, kdf_iterations, kdf_salt, password, keywrap256_ivlen, toQ_bufsize);
+        }
+        else if (strcmp(mode, decrypt_symbol) == 0) {
+            toQ_decrypt(infile_name, outfile_name, toQ_key_length, toQ_nonce_length, toQ_mac_length, kdf_iterations, kdf_salt, password, keywrap256_ivlen, toQ_bufsize);
         }
     }
     return 0;
